d0383c87724d5381c99da7e0c7d50b3ebe0e48c9
Tomas Perez <tomasitopg6@gmail.com>
2021-06-02 06:19:24 +0000

    done

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response
+  if (distance <= 400) {
+    response = 'This one is on me!'
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    response = 'No can do.'
+  }
+  return response
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const response = city === 'NYC' ? "Ok, sounds good." : "No go."
+
+  return response;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch(tip) {
+  case 'generous':
+    response = "Thank you so much."
+    break;
+  case 'not as generous':
+    response = "Thank you."
+    break;
+  default:
+    response = 'Bye.'
+  };
+
+  return response;
 }
